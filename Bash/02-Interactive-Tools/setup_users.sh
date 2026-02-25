97f0a048e8695dc42a1062d46d7993ac9340d0cd
Blu3-Sky-Max <thebigshadow001@gmail.com>
2026-02-25 05:50:40 -0800

    feat: added setup_users.sh script to automate user setup, group assignment, and directory permissions

---
+++ Bash/02-Interactive-Tools/setup_users.sh
@@ -0,0 +1,51 @@
+#!/bin/bash 
+
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <username1> <username2> <username3>"
+    exit 1
+fi
+user="$@" 
+group="scriptgroup" 
+dir="documents projects archives" 
+share="/shared/teamfiles"
+echo "creating group: $group" 
+sudo  groupadd $group
+
+for i in $user ; do 
+  echo "creating user: $i"  
+  echo 
+      sudo useradd -m  -d /home/$i  $i ;
+	echo "creating subdir for user: $i"
+       echo 	
+   for j in $dir ; do 
+	sudo mkdir -p  /home/$i/$j 
+done 
+ echo "Adding $i to $group"
+ echo 
+ sudo  usermod -aG $group $i 
+done 
+ echo "Making a dir called $share " 
+ echo 
+ sudo  mkdir -p  $share 
+  echo "changing $share group to $group"
+ echo
+sudo chgrp -R $group  $share 
+ echo "changing permission for dir $share" 
+sudo chmod -R 2770 $share
+ 
+echo 
+# --- Summary Report --------------------------
+echo -e "\n--- SCRIPT COMPLETE: SUMMARY REPORT ---"
+echo "Group '$group' members:"
+getent group $group
+
+echo -e "\nPermissions for $share directory:"
+
+ls -ldn $share
+
+for i in ${user[@]}; do 
+echo -e "\nHome directory structure for users: $i"
+ sudo ls -l /home/$i 
+done 
+
+  
