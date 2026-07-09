0508825b2dea5ed5b9421903c20a5ea513aa1c50
Blu3-Sky-Max <thebigshadow001@gmail.com>
2026-07-09 14:50:03 -0700

    feat(foundation): add Kochan (GCD via Euclidean algorithm)

---
+++ C/Foundation/03-Kochan/Great_Common_Divisor.c
@@ -0,0 +1,29 @@
+#include <stdio.h> 
+
+/* 
+Euclidean algorithm 
+
+Helps to find the greatest common divisor(gcd)  between two non-negative integer values 
+
+usage: gcc Great_Common_Divisor.c 
+*/ 
+
+int main() { 
+
+ int i , j, result; 
+
+printf("Enter two non-negative number: "); 
+scanf("%i%i",&i,&j);
+
+while ( j != 0){ 
+
+result = i % j ; 
+  i = j;
+  j= result;  
+} 
+
+ printf("The greatest common  divisor of this number is: %d\n", i) ; 
+
+
+
+} 
