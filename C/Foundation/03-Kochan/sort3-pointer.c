7c613f6fc65f3256fa44279bd67c99701adcd8b9
Blu3-Sky-Max <thebigshadow001@gmail.com>
2026-08-05 18:55:20 -0700

    doc(sort-pointer.c): add sorted method using pointer to check the three variables

---
+++ C/Foundation/03-Kochan/sort3-pointer.c
@@ -0,0 +1,50 @@
+#include <stdio.h> 
+
+ // sorting interger without an array  
+ 
+void Sort3(int *input1, int*input2, int * input3){ 
+     int holder;  
+     
+  
+   if ( *input1 > *input2){ 
+        holder= *input2; 
+        *input2=*input1;
+        *input1=holder; 
+        
+   } 
+   
+   
+   if ( *input2 > *input3){ 
+   holder=*input3;
+   *input3=*input2;
+   *input2=holder; 
+   
+   } 
+
+  if ( *input1 > *input2){ 
+        holder= *input2; 
+        *input2=*input1;
+        *input1=holder; 
+   } 
+   
+     
+    
+    
+    
+    
+} 
+
+
+int main (){ 
+    
+  
+  int x,y,z; 
+  printf("%s", "Enter your your number: "); 
+  scanf("%i %i %i", &x,&y,&z); 
+    
+    Sort3(&x,&y,&z); 
+    
+    printf("sorted integer: %i %i %i", x,y,z); 
+    
+    
+} 
